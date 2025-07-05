d0bd17f716ceaf59ba7ddd486bb679632b729fee
hauwajibrilibrahim <hauwajibrilibrahim@gmail.com>
2025-07-05 20:30:37 +0100

    enhanced tests to cover loading and error states.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders loading spinner', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/loading/i)).toBeInTheDocument();
+});
+
+test('renders characters list', async () => {
+  render(<App />);
+  await waitFor(() => {
+    expect(screen.getByText(/Rick/i)).toBeInTheDocument();
+  });
+});
+test('renders error message on fetch failure', async () => {
+  global.fetch = jest.fn(() =>
+    Promise.reject(new Error('Failed to fetch characters'))
+  );
+
+  render(<App />);
+  await waitFor(() => {
+    expect(screen.getByText(/Failed to fetch characters/i)).toBeInTheDocument();
+  });
 });
